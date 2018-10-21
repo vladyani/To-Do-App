b963cf47afb1e902ec593eef98d3d0bf5bc3ee89
vladyani <vladyslavyantsov@gmail.com>
2018-10-21 10:54:14 +0100

    to do form template added

--- src/components/ToDoForm/ToDoForm.jsx
+++ src/components/ToDoForm/ToDoForm.jsx
@@ -1,10 +1,25 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import colors from './toDoForm.service';
 
 export default class ToDoForm extends Component {
+    constructor() {
+        this.state = {
+
+        }
+    }
     render() {
         return (
             <React.Fragment>
-                Hello ToDoForm
+                <form class="note-form" action="">
+                    <input type="text" name="subject" placeholder="Subject" data-error="Enter topic, please!" required />
+                    <input type="date" name="deadline" value="" placeholder="Date" data-error="Enter date, please!" required />
+                    <select name="category" data-error="Select category, please!" required>
+                        <option hidden disabled selected value>Category</option>
+
+                    </select>
+                    <textarea name="description" placeholder="What do you need to do?" data-error="Complete the field, please!" required></textarea>
+                    <button type="submit" class="add-note-btn"></button>
+                </form>
             </React.Fragment>
         );
     }
---
+++ src/components/ToDoForm/toDoForm.service.js
@@ -0,0 +1,18 @@
+export const colors = [
+    {
+        className: '',
+        label: ''
+    },
+    {
+        className: 'red',
+        label: 'Red Circle'
+    },
+    {
+        className: 'yellow',
+        label: 'Yellow Circle'
+    },
+    {
+        className: 'green',
+        label: 'Green Circle'
+    }
+];
