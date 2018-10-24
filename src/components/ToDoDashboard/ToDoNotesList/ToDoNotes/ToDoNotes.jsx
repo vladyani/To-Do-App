f65785484a7c47fb3a53a2c1788abadf00efb86c
Vlad Yantsov <vlad.yantsov@empirica.pl>
2018-10-24 17:53:42 +0200

    structure updated

--- src/components/ToDoDashboard/ToDoNotesList/ToDoNotes/ToDoNotes.jsx
+++ src/components/ToDoDashboard/ToDoNotesList/ToDoNotes/ToDoNotes.jsx
@@ -1,36 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const ToDoNotes = props => {
+    const {deadline, subject, description, priority} = props.toDoNote;
     return (
-        <React.Fragment>
-            {props.toDoNotes ?
-                props.toDoNotes.map((note, index) => {
-                    return (
-                        <div className="note-container" key={index}>
-                            <div className="note-body">
-                                <span className="note-btn-wrapper">
-                                    <button className="btn-transparent">
-                                        <span className="icon-trash"></span>
-                                    </button>
-                                </span>
-                                <h3>{note.deadline}</h3>
-                                <h3>{note.subject}</h3>
-                                <span className="note-btn-details-wrapper">
-                                    <button className="btn-transparent note-btn-icon"
-                                        style={{ background: "#fc4d59" }}>
-                                        <span className="icon-edit"></span>
-                                    </button>
-                                    <button className="btn-transparent note-btn-icon"
-                                        style={{ background: "#fc4d59" }}>
-                                        <span className="icon-check"></span>
-                                    </button>
-                                </span>
-                            </div>
-                        </div>
-                    )
-                }) : null}
-        </React.Fragment>
+        <div className="note-container">
+            <div className="note-body">
+                <span className="note-btn-wrapper">
+                    <button className="btn-transparent">
+                        <span className="icon-trash"></span>
+                    </button>
+                </span>
+                <h3>{deadline}</h3>
+                <h3>{subject}</h3>
+                <span className="note-btn-details-wrapper">
+                <button className="btn-transparent note-btn-icon"
+                        style={{background: "#fc4d59"}}>
+                    <span className="icon-edit"></span>
+                </button>
+                <button className="btn-transparent note-btn-icon"
+                        style={{background: "#fc4d59"}}>
+                    <span className="icon-check"></span>
+                </button>
+            </span>
+            </div>
+        </div>
     )
-}
+};
 
 export default ToDoNotes;
--- src/components/ToDoDashboard/ToDoNotesList/ToDoNotesList.jsx
+++ src/components/ToDoDashboard/ToDoNotesList/ToDoNotesList.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import ToDoNotes from './ToDoNotes/ToDoNotes';
 import LocalStorageService from '../../../common/service/localStorageService';
 
@@ -6,7 +6,8 @@ export default class ToDoNotesList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            toDoNotes: []
+            toDoNotes: [],
+            page: 0
         }
     }
 
@@ -18,15 +19,30 @@ export default class ToDoNotesList extends Component {
         this.setState({
             toDoNotes: LocalStorageService.findNotes()
         })
-    }
+    };
+
+    showNextPage = () => {
+        this.setState({
+            page: this.state.page + 1
+        })
+    };
+
+    showPreviousPage = () => {
+        this.setState({
+            page: this.state.page - 1
+        })
+    };
 
     render() {
         return (
             <React.Fragment>
+                <h1>{this.state.page}</h1>
                 <div className="note-list-container">
+                    <span><button onClick={this.showPreviousPage} disabled={!this.state.page}>previous</button></span>
                     <div className="note-list-wrapper">
-                        <ToDoNotes toDoNotes={this.state.toDoNotes} />
+                        {this.state.toDoNotes.map((toDoNote, index) => <ToDoNotes toDoNote={toDoNote} key={index}/>)}
                     </div>
+                    <span><button onClick={this.showNextPage}>next</button></span>
                 </div>
             </React.Fragment>
         )
