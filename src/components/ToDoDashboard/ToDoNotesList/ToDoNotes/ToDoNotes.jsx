5e81de3cedba0e2db450c7c0e47a689c084ef4f7
Vlad Yantsov <vlad.yantsov@empirica.pl>
2018-10-24 19:37:26 +0200

    color, border added to notes

--- src/components/ToDoDashboard/ToDoNotesList/ToDoNotes/ToDoNotes.jsx
+++ src/components/ToDoDashboard/ToDoNotesList/ToDoNotes/ToDoNotes.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ToDoNotes = props => {
     const {deadline, subject, description, priority} = props.toDoNote;
     return (
-        <div className="note-container">
+        <div className="note-container" style={{boxShadow: `0 0 0 1px ${priority}`}}>
             <div className="note-body">
                 <span className="note-btn-wrapper">
                     <button className="btn-transparent">
@@ -14,11 +14,11 @@ const ToDoNotes = props => {
                 <h3>{subject}</h3>
                 <span className="note-btn-details-wrapper">
                 <button className="btn-transparent note-btn-icon"
-                        style={{background: "#fc4d59"}}>
+                        style={{background: priority}}>
                     <span className="icon-edit"></span>
                 </button>
                 <button className="btn-transparent note-btn-icon"
-                        style={{background: "#fc4d59"}}>
+                        style={{background: priority}}>
                     <span className="icon-check"></span>
                 </button>
             </span>
