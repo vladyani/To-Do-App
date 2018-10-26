aa02d2b465c70cb16b4cb30c0ee8dcb955b12733
Vlad Yantsov <vlad.yantsov@empirica.pl>
2018-10-26 20:17:48 +0200

    update

--- src/common/components/ToDoSortNotes/ToDoSortNotes.jsx
+++ src/common/components/ToDoSortNotes/ToDoSortNotes.jsx
@@ -4,19 +4,18 @@ import options from './toDoSort.service';
 // TODO const { Option } = Select;
 
 export default class ToDoSort extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            notes: [],
             direction: "",
-            typeOfSort: "",
+            typeOfSort: ""
         }
     }
 
     sortByDeadline = (key) => {
         this.setState({
             toDoNotes: this.props.notesToSort.sort((a, b) =>
-                 this.state.direction === 'asc' ? new Date(a[key]) - new Date(b[key]) : new Date(b[key]) - new Date(a[key])
+                this.state.direction === 'asc' ? new Date(a[key]) - new Date(b[key]) : new Date(b[key]) - new Date(a[key])
             ),
         });
     };
@@ -27,7 +26,7 @@ export default class ToDoSort extends Component {
                 console.log(a[key]);
                 return this.state.direction === 'asc' ? a[key] - b[key] : b[key] - a[key];
             })
-        })  
+        })
     }
 
     handleChange = (e) => {
@@ -36,26 +35,26 @@ export default class ToDoSort extends Component {
             direction: e.target[index].getAttribute('data-way'),
             typeOfSort: e.target[index].getAttribute('data-type')
         }, () => {
-        this.state.typeOfSort === "date" ? this.sortByDeadline("deadline") : this.sortByPriority("priorityId");
-        this.props.stateSetter(this.props.notesToSort);
+            this.state.typeOfSort === "date" ? this.sortByDeadline("deadline") : this.sortByPriority("priorityId");
+            this.props.stateSetter(this.props.notesToSort);
         });
     }
-        
+
     render() {
-        console.log('sorter',this.props.notesToSort);
-        return(
+        console.log('sorter', this.props.notesToSort);
+        return (
             <React.Fragment>
-            <select  
-                placeholder="Sort By"
-                style={{width: "10rem"}}
-                onChange={e=>this.handleChange(e)}>
-                {options.map((option, index) => 
-                <option key={index} 
-                        value={option.value} 
-                        data-type={option.typeOfSort} 
-                        data-way={option.wayOfSort}>{option.value}</option>
-                )}
-            </select>
+                <select
+                    placeholder="Sort By"
+                    style={{width: "10rem"}}
+                    onChange={e => this.handleChange(e)}>
+                    {options.map((option, index) =>
+                        <option key={index}
+                                value={option.value}
+                                data-type={option.typeOfSort}
+                                data-way={option.wayOfSort}>{option.value}</option>
+                    )}
+                </select>
             </React.Fragment>
         )
     }
