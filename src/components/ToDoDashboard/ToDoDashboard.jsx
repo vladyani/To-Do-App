6f7f9c1b83c1b89b0a5bde162032c546d3ca250c
Vlad Yantsov <vlad.yantsov@empirica.pl>
2018-10-26 18:13:26 +0200

    tododashboard refactor

--- src/components/ToDoDashboard/ToDoDashboard.jsx
+++ src/components/ToDoDashboard/ToDoDashboard.jsx
@@ -1,14 +1,96 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ToDoNotesList from './ToDoNotesList/ToDoNotesList';
 import ToDoButton from '../../common/components/ToDoButton/ToDoButton';
+import LocalStorageService from "../../common/service/localStorageService";
+import { notification, Icon, Modal } from 'antd';
 
-const ToDoDashboard = () => (
-    <React.Fragment>
-        <ToDoNotesList/>
-        <div className="btn-wrapper">
-            <ToDoButton btnClass="add-note-btn" routeTo="/todoform"/>
-        </div>
-    </React.Fragment>
-);
+export default class ToDoDashboard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            toDoNotes: [],
+            page: 0,
+            itemsPerPage: 6
+        }
+    }
 
-export default ToDoDashboard;
+    componentDidMount() {
+        this.getNotes(this.state.itemsPerPage, this.state.page);
+    }
+
+    //force is optional argument, if you pass it then it will load all results from local storage object
+    getNotes = (itemsPerPage, page) => {
+        this.setState({
+            toDoNotes: LocalStorageService.findAndPaginateNotes(itemsPerPage, page)
+        })
+    };
+
+    showNextPage = () => {
+        const { itemsPerPage, page } = this.state;
+        this.setState({
+            page: page + 1
+        }, () => {
+            let pageNumber = page + 1;
+            console.log(pageNumber)
+            this.getNotes(itemsPerPage, pageNumber);
+        });
+    };
+
+    // openNotification = () => {
+    //     notification.open({
+    //         message: 'Notification Title',
+    //         description: 'This is the content of the notification. This is the content of the notification. This is the content of the notification.',
+    //         icon: <Icon type="smile" style={{color: '#108ee9'}}/>
+    //     });
+    // };
+
+    showPreviousPage = () => {
+        const { itemsPerPage, page } = this.state;
+        this.setState({
+            page: page - 1
+        }, () => {
+            let pageNumber = page - 1;
+            console.log(pageNumber)
+            this.getNotes(itemsPerPage, pageNumber);
+        });
+        // if(this.state.page) this.openNotification();
+    };
+
+    confirmDeleteNote = (noteId, isActive) => {
+        if (isActive) {
+            Modal.confirm({
+                title: 'Do you want to delete these To Do\'s?',
+                content: 'This note will not come back, make sure you do the right thing :)',
+                onOk: () => {
+                    this.deleteNote(noteId);
+                },
+                onCancel: () => {
+                },
+            });
+        } else {
+            this.deleteNote(noteId);
+        }
+    };
+
+    deleteNote = (noteId) => {
+        LocalStorageService.deleteNote(noteId);
+        this.getNotes(this.state.itemsPerPage, this.state.page);
+    };
+
+    render() {
+        const {toDoNotes,page} = this.state;
+
+        return (
+            <React.Fragment>
+                <ToDoNotesList toDoNotes={toDoNotes}
+                               page={page}
+                               showPreviousPage={this.showPreviousPage}
+                               showNextPage={this.showNextPage}
+                               confirmDeleteNote={this.confirmDeleteNote}/>
+                <div className="btn-wrapper">
+                    <ToDoButton btnClass="add-note-btn" routeTo="/todoform"/>
+                </div>
+            </React.Fragment>
+        )
+    }
+}
