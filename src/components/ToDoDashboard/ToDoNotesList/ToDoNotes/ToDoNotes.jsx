670f26687524363b5e4821f5aada4fae01dcf4d7
vladyani <vladyslavyantsov@gmail.com>
2018-10-29 18:59:53 +0000

    tooltips added

--- src/components/ToDoDashboard/ToDoNotesList/ToDoNotes/ToDoNotes.jsx
+++ src/components/ToDoDashboard/ToDoNotesList/ToDoNotes/ToDoNotes.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ToDoEditModal from '../../../../common/components/ToDoEditModal/ToDoEditModal';
 import {Tooltip} from 'antd';
-import{tooltipOptions} from '../../../../common/service/notificationService';
+import {tooltipOptions} from '../../../../common/service/notificationService';
 
 const ToDoNotes = props => {
     const {deadline, subject, priority, noteId, isActive} = props.toDoNote;
@@ -25,16 +25,22 @@ const ToDoNotes = props => {
                     <h3>{deadline}</h3>
                     <h3>{subject}</h3>
                     <span className="note-btn-details-wrapper">
+                        <Tooltip placement="leftTop"
+                                 title={tooltipOptions.editNote}>
                             <button className="btn-transparent note-btn-icon"
                                     onClick={props.toggleModal}
                                     style={{background: priority}}>
                                 <span className="icon-edit"></span>
                             </button>
+                            </Tooltip>
+                        <Tooltip placement="rightTop"
+                                 title={`${tooltipOptions.updateNoteStatus}${isActive ? 'completed' : 'in progress'}`}>
                             <button className="btn-transparent note-btn-icon"
                                     onClick={() => props.completedOrInProgressNote(noteId, isActive)}
                                     style={{background: priority}}>
                                 <span className="icon-check"></span>
                             </button>
+                         </Tooltip>
                     </span>
                 </div>
             </div>
