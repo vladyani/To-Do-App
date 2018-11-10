bb40a62fa2a16be56807603a046efa3f84964d36
vladyani <vladyslavyantsov@gmail.com>
2018-11-10 14:10:09 +0000

    bug fix

--- src/components/MainPage/ToDoCounter/ToDoCounter.jsx
+++ src/components/MainPage/ToDoCounter/ToDoCounter.jsx
@@ -6,7 +6,7 @@ export default class ToDoCounter extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            notes: false
+            notes: []
         }
     }
 
@@ -22,15 +22,16 @@ export default class ToDoCounter extends Component {
         const {notes} = this.state;
         return (
             <div className="bounceInDown animated">
-                {notes ? notes.length !== 0 ?
-                    <React.Fragment>
+                {notes ?
+                    notes.length !== 0 ?
+                        <React.Fragment>
                         <span>Seems that you have todo's, it's
                             <span
                                 className="text-brand number-length">{notes.length}</span> {notes.length === 1 ? 'note' : 'notes'}
                         </span>
-                        <p><Link to="/tododashboard" className="text-brand">Go to dashboard</Link></p>
-                    </React.Fragment>
-                    : 'Seems that you have no to-doo things!' : 'Seems that you have no to-doo things!'}
+                            <p><Link to="/tododashboard" className="text-brand">Go to dashboard</Link></p>
+                        </React.Fragment>
+                        : 'Seems that you have no to-doo things!' : 'Seems that you have no to-doo things!'}
             </div>
         );
     }
